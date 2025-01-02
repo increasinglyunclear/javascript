8c12f62a1747a9cbd25902b05d01bb1f679fb289
Increasingly unclear <kevinwalker1@mac.com>
2025-01-02 18:23:46 +0000

    Add files via upload

---
+++ randomwalk3d/Walker.js
@@ -0,0 +1,43 @@
+class Walker {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+    this.trail = [];
+    this.trail.push(createVector(this.x, this.y, this.z));
+  }
+  
+  step() {
+    this.x += random(-2, 2);
+    this.y += random(-2, 2);
+    this.z += random(-2, 2);
+    
+    this.x = constrain(this.x, -200, 200);
+    this.y = constrain(this.y, -200, 200);
+    this.z = constrain(this.z, -200, 200);
+    
+    this.trail.push(createVector(this.x, this.y, this.z));
+  }
+  
+  render() {
+    // Draw trail with transparency
+    stroke(255, 35);  // White with only 35/255 opacity
+    strokeWeight(1);
+    noFill();
+    
+    // Draw trail as continuous line
+    beginShape();
+    for (let p of this.trail) {
+      vertex(p.x, p.y, p.z);
+    }
+    endShape();
+    
+    // Draw current position with slight glow
+    push();
+    translate(this.x, this.y, this.z);
+    noStroke();
+    fill(255, 35);
+    sphere(0.01);
+    pop();
+  }
+} 
---
+++ randomwalk3d/sketch.js
@@ -0,0 +1,84 @@
+let walkers = [];
+let rotationY = 0;
+let rotationX = 0;
+let zoomLevel = 14;
+let targetZoom = 14;
+let zoomSpeed = 0.15;
+let zoomStep = 0.8;
+let zoomingIn = false;
+let zoomingOut = false;
+
+function setup() {
+  console.log('Setting up...');
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  perspective(PI/3.0, width/height, 0.00001, 5000);
+  
+  // Create multiple walkers
+  for (let i = 0; i < 8; i++) {
+    walkers.push(new Walker());
+  }
+  
+  // Remove loading indicator
+  let loading = document.getElementById('loading');
+  loading.classList.add('fade');
+  setTimeout(() => {
+    loading.style.display = 'none';
+  }, 500);
+}
+
+function draw() {
+  background(0);
+  
+  // Handle continuous zooming
+  if (zoomingIn) {
+    targetZoom = targetZoom - zoomStep;
+  }
+  if (zoomingOut) {
+    targetZoom = min(14, targetZoom + zoomStep);
+  }
+  
+  // Smooth zoom interpolation
+  zoomLevel = lerp(zoomLevel, targetZoom, zoomSpeed);
+  
+  // Calculate camera position based on mouse and zoom
+  rotationY = map(mouseX, 0, width, PI, -PI);
+  rotationX = map(mouseY, 0, height, PI, -PI);
+  rotationX = constrain(rotationX, -PI/2, PI/2);
+  
+  let camDist = pow(1.5, zoomLevel);
+  let camX = sin(rotationY) * cos(rotationX) * camDist;
+  let camY = sin(rotationX) * camDist;
+  let camZ = cos(rotationY) * cos(rotationX) * camDist;
+  
+  camera(camX, camY, camZ, 0, 0, 0, 0, 1, 0);
+  
+  // Basic lighting
+  ambientLight(50);
+  directionalLight(255, 255, 255, 0, 1, -1);
+  
+  // Update and draw all walkers
+  for (let walker of walkers) {
+    walker.step();
+    walker.render();
+  }
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    zoomingIn = true;
+  } else if (keyCode === DOWN_ARROW) {
+    zoomingOut = true;
+  }
+}
+
+function keyReleased() {
+  if (keyCode === UP_ARROW) {
+    zoomingIn = false;
+  } else if (keyCode === DOWN_ARROW) {
+    zoomingOut = false;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+} 
